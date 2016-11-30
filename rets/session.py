9364520de46a13005572e55d12b38e6190d6b383
josh <josha@refindly.com>
2016-11-30 15:56:00 -0500

    Wrote doc strings for all methods in Session object

--- rets/session.py
+++ rets/session.py
@@ -1,6 +1,7 @@
 from requests.auth import HTTPBasicAuth, HTTPDigestAuth
 import requests
-from rets.exceptions import MissingConfiguration, CapabilityUnavailable, MetadataNotFound, InvalidSearch
+from rets.exceptions import MissingConfiguration, CapabilityUnavailable, MetadataNotFound, InvalidSearch, \
+    UnexpectedParserType
 import logging
 from rets.interpreters.get_object import GetObject
 import re
@@ -77,6 +78,12 @@ class Session(object):
         self.add_capability(name='Login', uri=self.login_url)
 
     def add_capability(self, name, uri):
+        """
+        Add a capability of the RETS board
+        :param name: The name of the capability
+        :param uri: The capability URI given by the RETS board
+        :return: None
+        """
 
         parse_results = urlparse(uri)
         if parse_results.hostname is None:
@@ -114,11 +121,28 @@ class Session(object):
             return bulletin
 
     def get_preferred_object(self, resource, r_type, content_id, location=0):
+        """
+        Get the first object from a Resource
+        :param resource: The name of the resource
+        :param r_type: The type of object to fetch
+        :param content_id: The unique id of the item to get objects for
+        :param location: The path to get Objects from
+        :return: Object
+        """
         collection = self.get_object(resource=resource, r_type=r_type,
                                      content_ids=content_id, object_ids='0', location=location)
         return collection[0]
 
     def get_object(self, resource, r_type, content_ids, object_ids='*', location=0):
+        """
+        Get a list of Objects from a resource
+        :param resource: The resource to get objects from
+        :param r_type: The type of object to fetch
+        :param content_ids: The unique id of the item to get objects for
+        :param object_ids: ids of the objects to download
+        :param location: The path to get Objects from
+        :return: list
+        """
         request_ids = GetObject().ids(content_ids=content_ids, object_ids=object_ids)
 
         response = self.request(
@@ -147,14 +171,19 @@ class Session(object):
     def get_system_metadata(self):
         """
         Get the top level metadata
-        :return:
+        :return: SystemModel
         """
         parser = SystemParser(version=self.version)
-        return self.make_metadata_request(meta_type='METADATA-SYSTEM', meta_id=0, parser=parser)
+        return self.make_metadata_request(meta_id=0, parser=parser)
 
     def get_resources_metadata(self, resource_id=None):
+        """
+        Get resource metadata
+        :param resource_id: The name of the resource to get metadata for
+        :return: ResourceModel
+        """
         parser = ResourceParser()
-        result = self.make_metadata_request(meta_type='METADATA-RESOURCE', meta_id=0, parser=parser)
+        result = self.make_metadata_request(meta_id=0, parser=parser)
 
         if resource_id:
             for name, r in result.items():
@@ -165,34 +194,67 @@ class Session(object):
         return result
 
     def get_classes_metadata(self, resource_id):
+        """
+        Get classes for a given resource
+        :param resource_id: The resource name to get class metadata for
+        :return: ResourceClassModel
+        """
         parser = ResourceClassParser()
-        return self.make_metadata_request(meta_type='METADATA-CLASS', meta_id=resource_id, parser=parser)
+        return self.make_metadata_request(meta_id=resource_id, parser=parser)
 
     def get_table_metadata(self, resource_id, class_id):
+        """
+        Get metadata for a given resource: class
+        :param resource_id: The name of the resource
+        :param class_id: The name of the class to get metadata from
+        :return: TableModel
+        """
         parser = TableParser()
-        return self.make_metadata_request(meta_type='METADATA-TABLE', meta_id=resource_id + ':' + class_id, parser=parser)
+        return self.make_metadata_request(meta_id=resource_id + ':' + class_id, parser=parser)
 
     def get_object_metadata(self, resource_id):
+        """
+        Get object metadata from a resource
+        :param resource_id: The resource name to get object metadata for
+        :return: ObjectMetadataModel
+        """
         parser = ObjectParser()
-        return self.make_metadata_request(meta_type='METADATA-OBJECT', meta_id=resource_id, parser=parser)
+        return self.make_metadata_request(meta_id=resource_id, parser=parser)
 
     def get_lookup_values(self, resource_id, lookup_name):
+        """
+        Get possible lookup values for a given field
+        :param resource_id: The name of the resource
+        :param lookup_name: The name of the the field to get lookup values for
+        :return: LookUpTypeModel
+        """
         parser = LookupTypeParser()
-        return self.make_metadata_request(meta_type='METADATA-LOOKUP_TYPE', meta_id=resource_id + ':' + lookup_name, parser=parser)
+        return self.make_metadata_request(meta_id=resource_id + ':' + lookup_name, parser=parser)
 
-    def make_metadata_request(self, meta_type, meta_id, parser):
+    def make_metadata_request(self, meta_id, parser):
         """
         Get the Metadata
-        :param meta_type:
-        :param meta_id:
-        :param parser:
-        :return:
+        :param meta_id: The name of the resource, class, or lookup to get metadata for
+        :param parser: An instance of the parser to parser the response
+        :return: dict
         """
+        class_type_to_meta_type = {
+            ResourceClassParser: 'METADATA-CLASS',
+            TableParser: 'METADATA-TABLE',
+            ObjectParser: 'METADATA-OBJECT',
+            LookupTypeParser: 'METADATA-LOOKUP_TYPE',
+            SystemParser: 'METADATA-SYSTEM',
+            ResourceParser: 'METADATA-RESOURCE'
+        }
+
+        if class_type_to_meta_type.get(type(parser)) is None:
+            raise UnexpectedParserType("Unexpected parser type given: " + str(type(parser)))
+
         response = self.request(
             capability='GetMetadata',
             options={
                 'query': {
-                    'Type': meta_type,
+                    'Type': class_type_to_meta_type.get(type(parser)),
                     'ID': meta_id,
                     'Format': 'STANDARD-XML'
                 }
@@ -201,6 +263,16 @@ class Session(object):
         return parser.parse(response)
 
     def search(self, resource_id, class_id, search_filter=None, dmql_query=None, optional_parameters=None, recursive=False):
+        """
+        Preform a search on the RETS board
+        :param resource_id: The resource that contains the class to search
+        :param class_id: The class to search in
+        :param search_filter: The query as a dict
+        :param dmql_query: The query in dmql format
+        :param optional_parameters: Values for option paramters
+        :param recursive: if True, automatically account for offsets to get all data
+        :return: dict
+        """
         if not optional_parameters:
             optional_parameters = {}
 
@@ -256,10 +328,10 @@ class Session(object):
 
     def request(self, capability, options=None):
         """
-
-        :param capability:
-        :param options:
-        :return:
+        Make a request to the RETS server
+        :param capability: The name of the capability to use to get the URI
+        :param options: Options to put into the request
+        :return: Response
         """
         if options is None:
             options = {}
@@ -297,6 +369,10 @@ class Session(object):
         return response
 
     def user_agent_digest_hash(self):
+        """
+        Hash the user agent and user agent password
+        :return: md5
+        """
         ua_a1 = hashlib.md5('{0}:{1}'
                             .format(self.user_agent.strip(), self.user_agent_password.strip())
                             .encode('utf-8')).digest()
