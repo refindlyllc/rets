f64b7f968f32b2758337f52f0bc7bcf3bc987d7d
Matthew Crowson <matthew.d.crowson@gmail.com>
2017-02-23 08:26:06 -0500

    upping version

--- rets/__init__.py
+++ rets/__init__.py
@@ -1,7 +1,7 @@
 from .session import Session
 
 __title__ = 'rets'
-__version__ = '0.0.9'
+__version__ = '0.0.10'
 __author__ = 'REfindly'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2016 REfindly'
