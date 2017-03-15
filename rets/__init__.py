4a01fcbfb746368cce286b95eb301ece860c5b77
Matthew Crowson <matthew.d.crowson@gmail.com>
2017-03-15 15:01:32 -0400

    prep for release

--- rets/__init__.py
+++ rets/__init__.py
@@ -1,7 +1,7 @@
 from .session import Session
 
 __title__ = 'rets'
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 __author__ = 'REfindly'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2016 REfindly'
