5798053cf86a5baf2f1e533af587a292ca13322b
jenwadkins <jwadkins@homepartners.com>
2022-11-30 11:10:33 -0800

    cleanup repo for fork work

--- .github/PULL_REQUEST_TEMPLATE.md
+++
@@ -1,6 +0,0 @@
-## Description
-
-
-## GitHub Issues
-<!-- Proposed changes should be discussed in an issue before submitting a PR. -->
-<!-- Link to relevant tickets here. -->
--- setup.py
+++ setup.py
@@ -1,49 +1,25 @@
 import os
-import re
-from setuptools import setup
+from rets import __version__
+from setuptools import setup, find_packages
 
-# Set external files
-try:
-    from pypandoc import convert
-    README = convert('README.md', 'rst')
-except ImportError:
-    README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 
-with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
-    required = f.read().splitlines()
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-with open(os.path.join(os.path.dirname(__file__), 'test_requirements.txt')) as f:
-    test_required = f.read().splitlines()
-
-with open('rets/__init__.py', 'r') as fd:
-    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
-                        fd.read(), re.MULTILINE).group(1)
-
-if not version:
-    raise RuntimeError('Cannot find version information')
 
 setup(
-    name='rets',
-    version=version,
-    packages=['rets'],
-    install_requires=required,
-    tests_require=test_required,
-    test_suite='nose.collector',
+    name="rets",
+    version=__version__,
+    packages=["rets"],
+    url="https://github.com/homepartners/rets",
+    author="REfindly",
+    author_email="info@refindly.com",
+    description=("RETS Client for Real Estate Data"),
+    license="MIT License",
+    keywords="rets",
+    long_description=read("README.md"),
+    packages=find_packages(),
     include_package_data=True,
-    license='MIT License',
-    description='RETS Client for Real Estate Data',
-    long_description=README,
-    long_description_content_type='text/markdown',
-    url='https://github.com/refindlyllc/rets',
-    author='REfindly',
-    author_email='info@refindly.com',
-    classifiers=[
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ],
+    install_requires=["requests", "xmltodict", "six", "future"],
+    dependency_links=[],  # boilerplate for pipenv-setup check
 )
